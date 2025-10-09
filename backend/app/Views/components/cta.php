115b58aad463016c0bae2d18a7e80ca7e924d2f8
RH <aboboharvey7@gmail.com>
2025-10-09 18:01:55 +0800

    update(components): added CTA
    Updated landing page to use CTA file for tagline

---
+++ backend/app/Views/components/cta.php
@@ -0,0 +1,3 @@
+<p class="mb-8 max-w-xl text-gray-700 text-lg md:text-xl">
+    Discover delicious flavors, vibrant ambiance, and a taste of home. Join us for a memorable dining experience! :3c
+</p>
