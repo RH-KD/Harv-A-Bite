6abf4e6aedc302f5202026e2290024c4b13cbc23
RH <aboboharvey7@gmail.com>
2025-11-06 00:37:31 +0800

    feat(user table seeder): Added a seeder for users table

---
+++ backend/app/Database/Seeds/UsersSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    public function run(){
+        $now = date('Y-m-d H:i:s');
+        $password = password_hash('Password123!', PASSWORD_DEFAULT);
+        $data = [
+            [
+                'username'      => 'admin',
+                'email'         => 'admin@example.com',
+                'password_hash' => $password,
+                'created_at'    => $now,
+                'updated_at'    => $now,
+            ],
+            [
+                'username'      => 'user1',
+                'email'         => 'user1@example.com',
+                'password_hash' => $password,
+                'created_at'    => $now,
+                'updated_at'    => $now,
+            ],
+        ];
+
+        $this->db->table('userTable')->insertBatch($data);
+    }
+}
